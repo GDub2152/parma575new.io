9e3f588a695e49eb94de1df379d603940b16c782
GDub2152 <107120490+GDub2152@users.noreply.github.com>
2026-07-05 23:02:02 -0400

    survey 3

---
+++ assets/js/events/events.js
@@ -0,0 +1,14 @@
+const EVENTS = [
+  {
+    active: true,
+    title: "Parma 575 GMRS Club Summer Picnic",
+    date: "Sunday, July 26, 2026",
+    time: "11:00 AM",
+    location: "James Day Park – Big Creek Pavilion",
+    address: "11828 W. Pleasant Valley Road, Parma, OH 44134",
+    description: "Please RSVP so we can plan food, seating, and activities.",
+    buttonText: "RSVP Now",
+    buttonLink: "https://script.google.com/d/1NjdFYJ166ZvJ1lEabJGDtwo7Gdrwv6QSMjfrbufZJseHkFjh9ZjIMyo0/edit?usp=sharing",
+    image: "assets/images/picnic-rsvp-banner.png"
+  }
+];
